a6dccf3a09bf598071152512a207ee4c3ded0b74
saidmagdy1 <mandopop978@gmail.com>
2024-10-26 11:41:26 +0300

    First commit

---
+++ Socket_Server.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include <cstring>    // For memset
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>   // For close()
